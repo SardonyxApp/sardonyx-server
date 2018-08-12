f562dc8ebb181dafed3bd5b917e1292ab1230b5a
natsuozawa <natsuozawa@outlook.com>
2018-08-12 17:43:41 +0900

    set up basic server

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const app = express();
 
-const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({extended: false});
 
+const request = require('request');
+
+/* SERVE STATIC FILES */
 app.use(express.static('public'));
 
-app.get('/validate', (req, res) => {
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/public/login.html');
+});
+
+/* SERVE API */
+app.use('/api', (req, res, next) => {
   res.set('Access-Control-Allow-Origin', '*');
+  //allow from all for now, for testing purposes
+  next();
+})
+
+app.get('/api/web/validate', (req, res) => {
   res.sendStatus(200);
+});
+
+app.post('/api/web/login', urlencodedParser, (req, res) => {
+  request.post({
+    url: 'https://kokusaiib.managebac.com/sessions', 
+    form: req.body
+  }, (err, response, body) => {
+    if (response.caseless.dict.location) res.status(200).redirect('../..');
+    else res.status(401).redirect('../../login?invalid=true');
+  });
 });
 
 const listener = app.listen(process.env.PORT, () => {
