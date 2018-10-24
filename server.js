7352359c2fc8f106f304358a61de48d40bf64387
natsuozawa <natsuozawa@outlook.com>
2018-10-24 22:18:22 +0900

    Implement basic Managebac login

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser');
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const multer = require('multer');
+const upload = multer();
+//parse multipart/form-data
 
 const request = require('request');
-
-const jwt = require('jsonwebtoken');
 
 /* SERVE API */
 app.use('/api', (req, res, next) => {
@@ -25,7 +24,7 @@ app.get('/api/validate', (req, res) => {
   //invalidate all for now
 });
 
-app.post('/api/login', urlencodedParser, (req, res) => {
+app.post('/api/login', upload.array(), (req, res) => {
   request.post({
     url: 'https://kokusaiib.managebac.com/sessions',
     form: req.body
@@ -41,24 +40,18 @@ app.post('/api/login', urlencodedParser, (req, res) => {
       //extract necessary cookies from header to store on client
       const __cfdiud = response.headers['set-cookie'][0].split(';')[0];
       const _managebac_session = response.headers['set-cookie'][2].split(';')[0];
-      const payload = {
-        "Cookie": __cfdiud + '; ' + _managebac_session,
-        "iss": "sardonyx-server",
-        "sub": req.body.login,
-      };
-      jwt.sign(payload, process.env.SECRET_KEY, {
-        expiresIn: '30d'
-      }, (err, token) => {
-        if (err) {
-          console.error(err);
-          res.status(503).send('There was an error in authentication.');
-        }
-        res.set('set-cookie', `authorization_token=${token}; Expires=${new Date(Date.now() + 2592000000)}; Path="/"; Secure; HttpOnly; SameSite=Strict`);
-        res.status(200).redirect('../..');
+      const payload = JSON.stringify({
+        cfdiud: __cfdiud,
+        managebacSession: _managebac_session,
+        login: req.body.login,
+        pass: req.body.password
       });
+      res.status(200)
+      res.append('Login-Token', payload);
+      res.send();
     }
 
-    else res.status(401).redirect('../../login?invalid=true');
+    else res.sendStatus(401);
   });
 });
 
