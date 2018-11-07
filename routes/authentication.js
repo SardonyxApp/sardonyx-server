bb187bbe547a05919606c409e783668fc0ab1503
natsuozawa <natsuozawa@outlook.com>
2018-11-08 01:29:24 +0900

    Fix bugs, format, apply best practices

--- routes/authentication.js
+++ routes/authentication.js
@@ -2,31 +2,23 @@ const request = require("request");
 
 /**
  * @description Convert Login-Token header to req.body FormData
- * @param {Object} req
- * req should have the header 'Login-Token'
- * @param {Object} req
+ * @param {Object} req should have the header 'Login-Token'
+ * @param {Object} res
  * @param {Function} next
  */
 exports.loginTokenToBody = (req, res, next) => {
-
-  // Get the login token from header, otherwise set it to an empty dictionary
+  // Get the login token from header, otherwise set it to an empty object
   const credentials = JSON.parse(req.headers['login-token'] || '{}') || {};
 
   if (credentials.login && credentials.password) {
-
     req.body = req.body || {}; // Define req.body in case of GET requests
     req.body.login = credentials.login;
     req.body.password = credentials.password;
     next();
-
   } else {
-
-    // The request did not contain a login-token. Maybe they have not done an initial login yet?
-    // Unauthorize them before sending useless requests
+    // The request did not contain a login-token, unauthorize them before sending useless requests
     res.status(401).end();
-
   }
-
 };
 
 
@@ -36,19 +28,16 @@ exports.loginTokenToBody = (req, res, next) => {
  * @param {Object} res 
  * @param {Function} next
  */
-
 exports.loginToManagebac = (req, res, next) => {
-
-  let additionalFormData = {
+  const additionalFormData = {
     'remember_me': 1
   };
 
   // Relay POST request with 'login' and 'password' to ManageBac
   request.post({
     url: 'https://kokusaiib.managebac.com/sessions',
-    form: { ...req.body, ...additionalFormData } // Send combined data
+    form: { ...req.body, ...additionalFormData } // Combine data
   }, (err, response) => {
-
     if (err) {
       console.error(err);
       res.status(502).write('There was an error accessing Managebac.').end();
@@ -56,9 +45,8 @@ exports.loginToManagebac = (req, res, next) => {
     }
 
     // ManageBac returns a 302 redirection from /sessions to /student on success
-    // Check if the Destination is /student, then keep all the information to send back to client
-    if (response.caseless.dict.location.includes('/student')) {
-
+    // Check if the destination is /student, then keep all the information to send back to client
+    if (response.caseless.dict.location && response.caseless.dict.location.includes('/student')) {
       const __cfdiud = response.headers['set-cookie'][0].split(';')[0];
       const _managebac_session = response.headers['set-cookie'][2].split(';')[0];
       const login = req.body.login;
@@ -70,15 +58,11 @@ exports.loginToManagebac = (req, res, next) => {
         password: password
       });
       res.append('Login-Token', payload);
-      res.status(200); // Don't use sendStatus because that'll change Response to Finished
-      // See: https://stackoverflow.com/questions/7042340/error-cant-set-headers-after-they-are-sent-to-the-client
       return next();
-
     }
 
-    // Nonexistent or incorrect redirection => unauthorized
+    // Nonexistent or incorrect redirection, unauthorized
     res.status(401).end();
-
   });
 };
 
@@ -90,16 +74,13 @@ exports.loginToManagebac = (req, res, next) => {
  * @param {Object} res 
  */
 exports.createSardonyxToken = (req, res) => {
-
   // Create token
-  token = 'temporary0123abcd'
+  const token = 'temporary0123abcd'
 
   // Put token in DB
   // createDBEntry(req.body.login, token)
 
   // Return that token
   res.append('Sardonyx-Token', token);
-
-  res.end();
-
+  res.status(200).end();
 };
--- server.js
+++ server.js
@@ -8,28 +8,26 @@ const authRoutes = require('./routes/authentication');
 
 // Route to return a random response code of either 401 or 200
 app.all('/random', (req, res) => {
-
-  let random = Math.floor(Math.random() * 2); // Generate either 0 or 1
+  const random = Math.floor(Math.random() * 2); // Generate either 0 or 1
   res.sendStatus(random === 0 ? 401 : 200);
-
 });
 
 // Set general settings on all of the API routes
 app.use('/api', (req, res, next) => {
-
   // Allow from all for now, for testing purposes
   res.set('Access-Control-Allow-Origin', '*');
-
-  // Continue with processing the request
   next();
-
 });
 
 // Initial validation
-app.get('/api/validate', authRoutes.loginTokenToBody, authRoutes.loginToManagebac);
+app.get('/api/validate', authRoutes.loginTokenToBody, authRoutes.loginToManagebac, (req, res) => {
+  res.status(200).end();
+});
 
 // Reissue tokens
-app.get('/api/login', authRoutes.loginTokenToBody, authRoutes.loginToManagebac);
+app.get('/api/login', authRoutes.loginTokenToBody, authRoutes.loginToManagebac, (req, res) => {
+  res.status(200).end();
+});
 
 // Initial login
 // use upload.none() when it's only text fields
