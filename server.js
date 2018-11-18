886cf02abf06d8e8ba33b909378b819ca4b88435
natsuozawa <natsuozawa@outlook.com>
2018-11-18 17:33:21 +0900

    Set up front end React and Webpack

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const app = express();
 const multer = require('multer');
 const upload = multer(); // Used to parse multipart/form-data
 
+require('dotenv').config();
+
 const auth = require('./helpers/authentication');
+
+app.use(express.static('public'));
 
 // Route to return a random response code of either 401 or 200
 app.all('/random', (req, res) => {
