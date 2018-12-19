432c1bedf7f0ecd9480da05236e320f9cb8ba130
natsuozawa <natsuozawa@outlook.com>
2018-12-19 22:41:06 +0900

    Retrieve notification message from Managebac

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.get('/api/group/:resourceId/messages/:destinationId', auth.loginTokenToCooki
 
 // Load notifications 
 app.get('/api/notification', auth.loginTokenToCookie, auth.getResource('notifications'), mb.loadNotifications, end200);
-app.get('/api/notification/:resourceId', auth.loginTokenToCookie, auth.getResource('notifications'), end200);
+app.get('/api/notification/:resourceId', auth.loginTokenToCookie, auth.getResource('notifications'), mb.loadNotification, end200);
 module.exports = app;
 // app.js and server.js are split for testing reasons
