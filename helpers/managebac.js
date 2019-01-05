acc683f41f5b84aeeed52ef87b24a46fb4f5277f
natsuozawa <natsuozawa@outlook.com>
2019-01-06 01:06:27 +0900

    Failed attempt at sending messages

--- helpers/managebac.js
+++ helpers/managebac.js
@@ -4,12 +4,84 @@
  * @license MIT
  */
 
+const request = require('request');
 const cheerio = require('cheerio');
 const { getMonthFromAbbr, getMonth, guessFutureYear, guessPastYear, createDate } = require('./helpers');
 
 // Custom method for replacing \n characters 
 String.prototype.delNewlines = function() {
   return this.replace(/\n/g, '');
+};
+
+/**
+ * @description Create a Managebac URL
+ * @param {String} resource 
+ * resource can be any of the valid Managebac resources, like classes and groups
+ * @param {String} destination 
+ * destination can be any of the valid Managebac destinations, like assignments and messages
+ * @example loadUrl('classes') with no parameters will GET /student/classes
+ * @example loadUrl('classes') with req.params.resourceId will GET /student/classes/:classId
+ * @example loadUrl('classes', 'assignments') without req.params.destinationId will GET /student/classes/:resourceId/assignments
+ * @example loadUrl('classes', 'assignments') with req.params.destinationId will GET /student/classes/:resourceId/assignments/:destinationId
+ */
+exports.loadUrl = (resource, destination) => {
+  return (req, res, next) => {
+    req.url = `https://kokusaiib.managebac.com/student`;
+    if (resource && req.params.resourceId) {
+      if (destination) {
+        if (req.params.destinationId) req.url += `/${resource}/${req.params.resourceId}/${destination}/${req.params.destinationId}`;
+        else req.url += `/${resource}/${req.params.resourceId}/${destination}`;
+      } else {
+        req.url += `/${resource}/${req.params.resourceId}`;
+      }
+    } else if (resource) {
+      req.url += `/${resource}`;
+    }
+
+    if (req.query.pageId) req.url += `/page/${req.query.pageId}`;
+    else if (req.query.pageParam) req.url += `?page=${req.query.pageParam}`;
+    console.log(req.url);
+    next();
+  };
+};
+
+/**
+ * @description Retrieve any resource from Managebac
+ * @param {Object} req 
+ * @param {Object} res 
+ * @param {Function} next 
+ */
+exports.retrieve = (req, res, next) => {
+  const j = request.jar(); // Cookie jar 
+  j.setCookie(request.cookie(req.cookie.cfduid), 'https://kokusaiib.managebac.com');
+  j.setCookie(request.cookie(req.cookie.managebacSession), 'https://kokusaiib.managebac.com');
+  
+  request.get({
+    url: req.url,
+    jar: j,
+  }, (err, response) => {
+    if (err) {
+      console.error(err);
+      res.status(502).end();
+      return;
+    }
+
+    // Successfully returns class page 
+    if (response.statusCode === 200 && response.request.uri.href === req.url) {
+      const __cfduid = j.getCookieString('https://kokusaiib.managebac.com').split(';')[0];
+      const _managebac_session = j.getCookieString('https://kokusaiib.managebac.com').split(';')[1];
+      const payload = JSON.stringify({
+        cfduid: __cfduid,
+        managebacSession: _managebac_session
+      });
+      res.append('Login-Token', payload);
+      req.document = response.body;
+      return next();
+    }
+
+    //Nonexistent or invalid request, unauthorized 
+    res.status(401).end();
+  });
 };
 
 /**
@@ -475,6 +547,38 @@ exports.loadMessage = (req, res) => {
  * @param {Object} res 
  */
 exports.sendMessage = (req, res) => {
+  const j = request.jar(); // Cookie jar 
+  j.setCookie(request.cookie(req.cookie.cfduid), 'https://kokusaiib.managebac.com');
+  j.setCookie(request.cookie(req.cookie.managebacSession), 'https://kokusaiib.managebac.com');
+
+  req.body = JSON.parse(req.headers['message-data']);
+  console.log(req.body);
+
+  request.post({
+    url: req.url,
+    form: {
+      utf8: '✓',
+      _method: 'post',
+      'discussion[topic]': req.body.topic,
+      'discussion[body]': req.body.body,
+      'discussion[notify_via_email]': req.body.notifyViaEmail,
+      'discussion[private]': req.body.privateMessage, // API naming slightly changed, private is a reserved word in strict mode 
+      commit: 'Add Message'
+    },
+    jar: j,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    }
+  }, (err, response) => {
+    if (err) {
+      console.error(err);
+      res.status(502).end();
+      return;
+    }
+
+    console.log(response.statusCode);
+  });
+  
   res.status(200).end();
 }
 
