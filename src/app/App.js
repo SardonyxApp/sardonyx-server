2786e6cee07654b700af46ba83210c0af92a82bb
natsuozawa <natsuozawa@outlook.com>
2019-03-13 01:01:01 +0900

    Start work on web app UI

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,42 @@
+/**
+ * @fileoverview Front end app for Sardonyx tasklists.
+ * @author SardonyxApp
+ * @license MIT 
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './app.scss';
 
+import TopBar from './TopBar';
+import ContentWindow from './ContentWindow';
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: { // hard coded for now 
+        teacher: false,
+        name: 'John Doe', 
+        email: 'johndoe@example.com'
+      },
+      tasklist: {
+        name: 'Temporary title',
+        description: 'Test tasklist'
+      }
+    };
+  }
+
   render() {
-    return <div>This is the web app.</div>;
+    return (
+      <div>
+        <TopBar 
+          user={this.state.user}
+          tasklist={this.state.tasklist} 
+        />
+        <ContentWindow />
+      </div>
+    );
   }
 }
 
---
+++ src/app/components/User.js
@@ -0,0 +1,23 @@
+/**
+ * @fileoverview Component to render user information in the top bar.
+ * @author SardonyxApp
+ * @license MIT 
+ */
+
+import React from 'react';
+import { IconWhiteBackground } from '../../logos'; // use this as avatar for now
+
+class User extends React.Component {
+  render() {
+    return (
+      <div id="user">
+        <h3>{this.props.user.name}</h3>
+        <div id="avatar">
+          <IconWhiteBackground width={28} height={28} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default User;
