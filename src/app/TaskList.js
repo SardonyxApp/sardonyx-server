a3b1406b16008b25cd14f6075909a88b074ed719
natsuozawa <natsuozawa@outlook.com>
2019-03-16 00:26:59 +0900

    Show labels of each task

--- src/app/TaskList.js
+++ src/app/TaskList.js
@@ -8,22 +8,40 @@ import React from 'react';
 
 class TaskList extends React.Component {
   render() {
+    // Temporarily store values for comparison 
+    let store = null
+
     let tasks = this.props.tasks.sort((a, b) => {
       // convert null to "null" so that they come last
       if (String(a.due) < String(b.due)) return -1;
       else if (String(a.due) > String(b.due)) return 1;
       return 0; // They are equal
-    }).map((t, i) => (
-      <div 
-        className="card" 
-        key={t.id} 
-        onClick={() => this.props.onSelectTask(i)}
-        style={{ backgroundColor: this.props.currentTask === i ? '#f6e6dc' : ''}}
-      >
-        <h3 className="overview-title">{t.name}</h3>
-        <p className="overview-description">{t.description}</p>
-      </div>
-    ));
+    }).map((t, i) => {
+      // This gets messy here, but it was kept as a single component in order to refer back to store 
+      const view = (
+        <div>
+          <div className="side">
+            <p>{store === t.due ? null : new Intl.DateTimeFormat('en-US', { weekday: 'short' }).format(new Date(t.due))}</p>
+            <p>{store === t.due ? null : new Date(t.due).getDate()}</p>
+          </div>
+          <div 
+            className="card" 
+            key={t.id} 
+            onClick={() => this.props.onSelectTask(i)}
+            style={{ backgroundColor: this.props.currentTask === i ? '#f6e6dc' : ''}}
+          >
+            <h3 className="overview-title">{t.name}</h3>
+            <p className="overview-description">{t.description}</p>
+            <div className="overview-dots">
+              <div class="dot" style={{ backgroundColor: t.subject_color }}></div>
+              <div class="dot" style={{ backgroundColor: t.category_color }}></div>
+            </div>
+          </div>
+        </div>
+      );
+      store = t.due;
+      return view;
+    });
   
     return (
       <div id="tasklist" className="left-view">
---
+++ src/app/components/TaskLabels.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview Component to render task labels in info view.
+ * @author SardonyxApp
+ * @license MIT 
+ */
+
+import React from 'react';
+
+class TaskLabels extends React.Component {
+  render() {
+    const labels = [];
+    if (this.props.task.subject_id) {
+      labels.push(
+        <div className="label" style={{ background: this.props.task.subject_color}}>{this.props.task.subject_name}</div>
+      );
+    }
+    if (this.props.task.category_id) {
+      labels.push(
+        <div className="label" style={{ background: this.props.task.category_color}}>{this.props.task.category_name}</div>
+      );
+    }
+    
+    return (
+      <div class="task-labels">
+        {labels}
+      </div>
+    );
+  }
+}
+
+export default TaskLabels; 
