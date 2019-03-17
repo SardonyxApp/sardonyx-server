41a2171908ec7e7f88bc17f1425b014264e42a53
natsuozawa <natsuozawa@outlook.com>
2019-03-18 00:41:28 +0900

    Add more material icons

--- src/logos.js
+++ src/logos.js
@@ -75,6 +75,39 @@ class UserIcon extends React.Component {
   }
 }
 
+class SearchIcon extends React.Component {
+  render() {
+    return (
+      <svg viewBox="0 0 24 24" width={this.props.width || 24} height={this.props.height || 24}>
+        <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+        <path d="M0 0h24v24H0z" fill="none"/>
+      </svg>
+    );
+  }
+}
+
+class AddIcon extends React.Component {
+  render() {
+    return (
+      <svg viewBox="0 0 24 24" width={this.props.width || 24} height={this.props.height || 24}>
+        <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
+        <path d="M0 0h24v24H0z" fill="none"/>
+      </svg>
+    );
+  }
+}
+
+class CloseIcon extends React.Component {
+  render() {
+    return (
+      <svg viewBox="0 0 24 24" width={this.props.width || 24} height={this.props.height || 24}>
+        <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+        <path d="M0 0h24v24H0z" fill="none"/>
+      </svg>
+    );
+  }
+}
+
 class EditIcon extends React.Component {
   render() {
     return (
@@ -94,5 +127,8 @@ export {
   DescriptionIcon, 
   TimeIcon, 
   UserIcon, 
+  SearchIcon,
+  AddIcon,
+  RemoveIcon,
   EditIcon 
 };
