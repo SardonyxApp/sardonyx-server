81b8e2f2475b9cead766f7a38a7cdcf397cbe2ec
natsuozawa <natsuozawa@outlook.com>
2019-03-22 00:49:00 +0900

    Work on editing fields

--- src/app/components/TaskDescription.js
+++ src/app/components/TaskDescription.js
@@ -8,11 +8,57 @@ import React from 'react';
 import { DescriptionIcon } from '../../logos';
 
 class TaskDescription extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: false
+    };
+
+    this.textareaRef = React.createRef();
+
+    this.handleFocus = this.handleFocus.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  handleFocus() {
+    this.setState({
+      selected: true
+    });
+  }
+
+  handleBlur(e) {
+    this.props.onChangeTask('description', e.target.innerText);
+
+    this.setState({
+      selected: false 
+    });
+
+    // make request 
+  }
+
+  handleKeyDown(e) {
+    if (e.keyCode === 27) {
+      this.textareaRef.current.blur();
+      // This will trigger handleBlur
+    }
+  }
+
   render() {
     return (
       <div id="task-description" className="taskinfo-component">
         <DescriptionIcon />
-        <p>{this.props.description || 'No description provided.'}</p>
+        <p 
+          contentEditable
+          className="embed"
+          style={{ cursor: this.state.selected ? '' : 'pointer', borderBottom: this.state.selected ? '2px solid #2977b6' : '2px solid transparent' }}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
+          onKeyDown={this.handleKeyDown}
+          ref={this.textareaRef}
+        >
+          {this.props.description || 'No description provided.'}
+        </p>
       </div>
     );
   }
