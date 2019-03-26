9201e4ca3e3d9c6cb76cace0b9484504733c5b8f
natsuozawa <natsuozawa@outlook.com>
2019-03-27 00:30:01 +0900

    Implement UI for adding labels

--- src/app/modals/AddModal.js
+++ src/app/modals/AddModal.js
@@ -6,26 +6,48 @@
 
 import React from 'react';
 import { NextIcon } from '../../logos';
+import { TwitterPicker } from 'react-color';
 
 class AddModal extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      // Generate random hex color 
+      color: "#000000".replace(/0/g, () => Math.floor(Math.random() * 16).toString(16)) 
+    };
+
     this.inputRef = React.createRef();
     
     this.handleAddTask = this.handleAddTask.bind(this);
+    
+  }
+
+  validateName() {
+    const name = this.inputRef.current.value;
+    if (!name) {
+      this.inputRef.current.style.borderBottom = '2px solid #f44138';
+      this.inputRef.current.focus();
+      return false;
+    }
+    return true;
   }
 
   handleAddTask() {
-    const name = this.inputRef.current.value;
-    if (!name.length) {
-      this.inputRef.current.style.borderBottom = '2px solid #f44138';
-      this.inputRef.current.focus();
-      return;
+    if (this.validateName()) {
+      this.props.onCreateTask({ name });
+      this.props.onModal();
     }
+  }
 
-    this.props.onCreateTask({ name });
-    this.props.onModal();
+  handleAddLabel() {
+    if (this.validateName()) {
+      this.props.onModal();
+    }
+  }
+
+  handleColor(color) {
+    this.setState({ color });
   }
 
   render() {
@@ -35,8 +57,8 @@ class AddModal extends React.Component {
         <div id="add-modal" className="modal" style={{ right: this.props.modal.x, top: this.props.modal.y }}>
           <ul>
             <li onClick={() => this.props.onModal('add-task-modal', this.props.modal.x, this.props.modal.y)}>Add task</li>
-            <li>Add subject label</li>
-            <li>Add category label</li>
+            <li onClick={() => this.props.onModal('add-subject-modal', this.props.modal.x, this.props.modal.y)}>Add subject label</li>
+            <li onClick={() => this.props.onModal('add-category-modal', this.props.modal.x, this.props.modal.y)}>Add category label</li>
           </ul>
         </div>
       );
@@ -46,25 +68,70 @@ class AddModal extends React.Component {
     if (this.props.modal.name === 'add-task-modal') {
       return (
         <div id="add-task-modal" className="modal" style={{ right: this.props.modal.x, top: this.props.modal.y }}>
-          <input 
-            id="add-task-input" 
-            type="text" 
-            placeholder="Enter the name of the task"
-            autoFocus
-            onKeyDown={e => {
-              if (e.keyCode === 13) this.handleAddTask();
-              if (e.keyCode === 27) this.props.onModal();
-            }}
-            ref={this.inputRef}
-          />
-          <NextIcon 
-            width={16}
-            height={16}
-            style={{ cursor: 'pointer' }}
-            onClick={this.handleAddTask}
-          />
+          <div>
+            <input 
+              id="add-task-input" 
+              className="name-input"
+              type="text" 
+              style={{ width: '200px' }}
+              placeholder="Enter the name of the task"
+              autoFocus
+              onKeyDown={e => {
+                if (e.keyCode === 13) this.handleAddTask();
+                if (e.keyCode === 27) this.props.onModal();
+              }}
+              ref={this.inputRef}
+            />
+            <NextIcon 
+              width={16}
+              height={16}
+              style={{ cursor: 'pointer' }}
+              onClick={this.handleAddTask}
+            />
+          </div>
         </div>
       );
+    }
+
+    if (this.props.modal.name === 'add-subject-modal' || this.props.modal.name === 'add-category-modal') {
+      const type = this.props.modal.name === 'add-subject-modal' ? 'subject' : 'category';
+      return (
+        <div id={`add-${type}-modal`} className="modal" style={{ right: this.props.modal.x, top: this.props.modal.y }}>
+          <div>
+            <input
+              id={`add-${type}-input`}
+              className="name-input"
+              type="text"
+              style={{ width: '236px' }}
+              placeholder={`Enter the name of the ${type}`}
+              autoFocus
+              onKeyDown={e => {
+                if (e.keyCode === 13) this.handleAddLabel(type);
+                if (e.keyCode === 27) this.props.onModal();
+              }}
+              ref={this.inputRef}
+            />
+            <NextIcon 
+              width={16}
+              height={16}
+              style={{ cursor: 'pointer' }}
+              onClick={() => this.handleAddLabel(type)}
+            />
+          </div>
+
+          <TwitterPicker 
+            color={this.state.color}
+            onChangeComplete={color => this.handleColor(color.hex)}
+            width={252}
+            triangle="hide"
+          />
+
+          <div>
+            <p style={{ display: 'inline-block' }}>Use color</p>
+            <div className="dot" style={{ background: this.state.color, marginLeft: '4px' }}></div>
+          </div>
+        </div>
+      )
     }
 
     return null;
