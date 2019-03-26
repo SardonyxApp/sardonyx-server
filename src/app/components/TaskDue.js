ecab7d2313dab58116826a34fc25d9dd49c8e382
natsuozawa <natsuozawa@outlook.com>
2019-03-26 18:34:01 +0900

    Add ability to remove due date

--- src/app/components/TaskDue.js
+++ src/app/components/TaskDue.js
@@ -5,7 +5,7 @@
  */
 
 import React from 'react';
-import { TimeIcon } from '../../logos';
+import { TimeIcon, RemoveIcon } from '../../logos';
 
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
@@ -17,12 +17,18 @@ class TaskDue extends React.Component {
         <TimeIcon />
         <DatePicker
           selected={this.props.due}
-          onChange={date => this.props.onChangeTask({ due: date.toISOString() })}
+          onChange={date => this.props.onChangeTask({ due: date.toISOString()})}
           showTimeSelect
           timeFormat="HH:mm"
           timeIntervals={30}
           dateFormat="MMMM d, yyyy h:mm aa"
           placeholderText="No due date set."
+        />
+        <RemoveIcon 
+          width={16}
+          height={16}
+          style={{ display: this.props.due ? '' : 'none' }}
+          onClick={() => this.props.onChangeTask({ due: null })}
         />
       </div>
     );
