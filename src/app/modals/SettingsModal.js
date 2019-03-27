bd039aad496f7a87635964b8ee69f9c128ef2adb
natsuozawa <natsuozawa@outlook.com>
2019-03-27 19:54:28 +0900

    Work on interface to edit labels

--- src/app/modals/SettingsModal.js
+++ src/app/modals/SettingsModal.js
@@ -7,8 +7,58 @@
 import React from 'react';
 import { RemoveIcon, UserIcon, TasklistIcon, InfoIcon, LabelIcon } from '../../logos';
 
+class Label extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: false
+    };
+
+    this.textRef = React.createRef();
+  }
+
+  handleFocus() {
+    this.setState({
+      selected: true
+    });
+  }
+
+  handleBlur() {
+    this.setState({
+      selected: false
+    });
+  }
+
+  handleKeyDown(e) {
+    if (e.keyCode === 27) {
+      this.textRef.current.blur();
+    }
+  }
+
+  render() {
+    return (
+      <div
+        className="label"
+        key={this.props.label.name}
+        style={{ backgroundColor: this.props.label.color, cursor: 'pointer', padding: '4px 12px' }} // Modifications to .label divs in modal
+      >
+        <p contentEditable>{this.props.label.name || '\n'}</p>
+        <RemoveIcon 
+          height={20}
+          width={20}
+          style={{ marginLeft: '4px', fill: '#fff' }}
+          // onClick={/*handle delete*/}
+        />
+      </div>
+    );
+  }
+}
+
 class SettingsModal extends React.Component {
   render() {
+    const subjects = this.props.subjects.map(label => <Label label={label}/>);
+    const categories = this.props.categories.map(label => <Label label={label}/>);
+
     return this.props.modal.name === 'settings' ? (
       <div id="settings-modal" className="modal">
         <RemoveIcon />
@@ -28,8 +78,18 @@ class SettingsModal extends React.Component {
           <div className="setting">
             <div className="heading">
               <LabelIcon />
-              <h3 className="subheading">Labels</h3>
+              <h3 className="subheading">Subject Labels</h3>
+              <p>These are used to indicate the subject of the task. (e.g. Mathematics, English)</p>
             </div>
+            {subjects}
+          </div>
+          <div className="setting">
+            <div className="heading">
+              <LabelIcon />
+              <h3 className="subheading">Category Labels</h3>
+              <p>These are used to indicate the type of the work. (e.g. Homework, Exam Preparation)</p>
+            </div>
+            {categories}
           </div>
           <div className="heading">
             <UserIcon width={36} height={36} />
