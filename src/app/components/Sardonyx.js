0f44cfc299f46d50d06375da0376f0a7878a1456
natsuozawa <natsuozawa@outlook.com>
2019-03-27 17:46:44 +0900

    Add logos to ProfileModal

--- src/app/components/Sardonyx.js
+++ src/app/components/Sardonyx.js
@@ -10,7 +10,7 @@ import { IconInverted } from '../../logos';
 class Sardonyx extends React.Component {
   render() {
     return (
-      <div id="sardonyx">
+      <div id="sardonyx" style={{ cursor: 'pointer' }} onClick={() => window.location.replace('/')}>
         <IconInverted width={32} height={32}/>
         <h1 style={{ lineHeight: '36px', fontSize: '24px', marginLeft: '4px' }}>Sardonyx</h1>
       </div>
--- src/logos.js
+++ src/logos.js
@@ -60,7 +60,7 @@ class IconInverted extends React.Component {
   }
 }
 
-// Material icons 
+// Material icons (normal)
 
 /**
  * @description Tag icon
@@ -310,6 +310,69 @@ class NextIcon extends React.Component {
   }
 }
 
+/**
+ * @description Language icon
+ */
+class GlobeIcon extends React.Component {
+  render() {
+    return (
+      <svg 
+        className="globe-icon material-icon action" 
+        viewBox="0 0 24 24" 
+        width={this.props.width || 24} 
+        height={this.props.height || 24}
+        style={this.props.style}
+        onClick={this.props.onClick}
+      >
+        <path fill="none" d="M0 0h24v24H0V0z"/>
+        <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zm6.93 6h-2.95c-.32-1.25-.78-2.45-1.38-3.56 1.84.63 3.37 1.91 4.33 3.56zM12 4.04c.83 1.2 1.48 2.53 1.91 3.96h-3.82c.43-1.43 1.08-2.76 1.91-3.96zM4.26 14C4.1 13.36 4 12.69 4 12s.1-1.36.26-2h3.38c-.08.66-.14 1.32-.14 2s.06 1.34.14 2H4.26zm.82 2h2.95c.32 1.25.78 2.45 1.38 3.56-1.84-.63-3.37-1.9-4.33-3.56zm2.95-8H5.08c.96-1.66 2.49-2.93 4.33-3.56C8.81 5.55 8.35 6.75 8.03 8zM12 19.96c-.83-1.2-1.48-2.53-1.91-3.96h3.82c-.43 1.43-1.08 2.76-1.91 3.96zM14.34 14H9.66c-.09-.66-.16-1.32-.16-2s.07-1.35.16-2h4.68c.09.65.16 1.32.16 2s-.07 1.34-.16 2zm.25 5.56c.6-1.11 1.06-2.31 1.38-3.56h2.95c-.96 1.65-2.49 2.93-4.33 3.56zM16.36 14c.08-.66.14-1.32.14-2s-.06-1.34-.14-2h3.38c.16.64.26 1.31.26 2s-.1 1.36-.26 2h-3.38z"/>
+      </svg>
+    );
+  }
+}
+
+/**
+ * @description Lock icon 
+ */
+class LockIcon extends React.Component {
+  render() {
+    return (
+      <svg 
+        className="globe-icon material-icon action" 
+        viewBox="0 0 24 24" 
+        width={this.props.width || 24} 
+        height={this.props.height || 24}
+        style={this.props.style}
+        onClick={this.props.onClick}
+      >
+        <path d="M0 0h24v24H0z" fill="none"/>
+        <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2z"/>
+      </svg>
+    );
+  }
+}
+
+/**
+ * @description Exit icon (arrow pointing towards square)
+ */
+class ExitIcon extends React.Component {
+  render() {
+    return (
+      <svg 
+        className="globe-icon material-icon action" 
+        viewBox="0 0 24 24" 
+        width={this.props.width || 24} 
+        height={this.props.height || 24}
+        style={this.props.style}
+        onClick={this.props.onClick}
+      >
+        <path d="M0 0h24v24H0z" fill="none"/>
+        <path d="M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/>
+      </svg>
+    );
+  }
+}
+
 export { 
   Icon, 
   IconWhiteBackground, 
@@ -325,5 +388,8 @@ export {
   EditIcon,
   DeleteIcon,
   AddToListIcon,
-  NextIcon
+  NextIcon,
+  GlobeIcon,
+  LockIcon,
+  ExitIcon
 };
