61560561fd3af69df57980cea3105e0a0047b1b4
natsuozawa <natsuozawa@outlook.com>
2019-06-01 19:58:10 +0900

    Automatically redirect to app

--- src/app/components/Sardonyx.js
+++ src/app/components/Sardonyx.js
@@ -8,7 +8,7 @@ import React from 'react';
 import { IconInverted } from '../../logos';
 
 const Sardonyx = () => (
-  <div id="sardonyx" style={{ cursor: 'pointer' }} onClick={() => window.location.replace('/')}>
+  <div id="sardonyx" style={{ cursor: 'pointer' }} onClick={() => window.location.replace('/?redirect=false')}>
     <IconInverted width={32} height={32} />
     <h1 style={{ lineHeight: '36px', fontSize: '24px', marginLeft: '4px' }}>Sardonyx</h1>
   </div>
