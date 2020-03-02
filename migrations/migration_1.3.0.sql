e18e1dd03504f346bf1c970bea88f6d45c77fe21
natsuozawa <natsuozawa@outlook.com>
2020-03-02 14:21:34 +0900

    Finish migrating the database

---
+++ migrations/migration_1.3.0.sql
@@ -0,0 +1,37 @@
+-- This migration is for databases that were constructed using setup.sql prior to v1.3.0
+-- Log into MySQL, select the database, then execute:
+-- source /path/to/this/file
+
+-- NOTICE: this action is irreversible
+
+-- Drop the foreign key and column in tasks associated with students
+ALTER TABLE tasks DROP FOREIGN KEY tasks_ibfk_2;
+ALTER TABLE tasks DROP COLUMN student_id;
+
+-- Drop the foreign key and column in user_labels associated with students
+-- User labels contains the user's default label preferences
+ALTER TABLE user_labels DROP FOREIGN KEY user_labels_ibfk_1;
+ALTER TABLE user_labels DROP COLUMN student_id;
+
+-- Drop students, as Managebac integration is deprecated.
+DROP TABLE students;
+
+-- Rename the column in tasks
+ALTER TABLE tasks CHANGE COLUMN teacher_id user_id INT;
+
+-- Rename the column in user_labels
+ALTER TABLE user_labels CHANGE COLUMN teacher_id user_id INT;
+
+-- The foreign keys will be updated automatically. 
+-- The name will still contain the word teacher instead of user
+
+-- All accounts will now be the same type, having the same functionality as teacher accounts
+RENAME TABLE teachers TO users;
+
+-- Remove related managebac page link
+-- This was used by managebacLoader to examine whether an assignment was already added
+-- This does not remove any links in the task's description
+ALTER TABLE tasks DROP COLUMN managebac;
+
+-- Remove the related managebac class link
+ALTER TABLE subjects DROP COLUMN managebac;
