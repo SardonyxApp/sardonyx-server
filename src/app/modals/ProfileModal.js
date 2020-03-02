50c4b3b6ba48dfc660cd3f6d47fae698da683b77
natsuozawa <natsuozawa@outlook.com>
2020-03-02 16:59:23 +0900

    Delete any managebac references

--- src/app/modals/ProfileModal.js
+++ src/app/modals/ProfileModal.js
@@ -12,10 +12,6 @@ class ProfileModal extends React.Component {
         </div>
         <hr />
         <div>
-          <GlobeIcon width={20} height={20} onClick={() => window.open('https://kokusaiib.managebac.com', '_blank')} />
-          <p><a href="https://kokusaiib.managebac.com" target="_blank">Open Managebac</a></p>
-        </div>
-        <div>
           <LockIcon width={20} height={20} onClick={() => window.location.href = '/password'} />
           <p><a href="/password">Change Password</a></p>
         </div>
